d7293647172001f4e1a68766a41f6f32679dd701
wnag <wanglunia@sina.com>
2020-03-18 11:56:55 +0800

    update es8 practice

--- js/es8_await.js
+++ js/es8_await.js
@@ -1,5 +1,5 @@
 //使用await代替then操作
-//await必须跟随async使用
+//await必须跟随async使用，也就是说函数内部如果有await的话，一定要在这个函数前加async，注意不是定义promise的函数前加，而是在使用await的函数中加。
 //async和await其实是Promise的语法糖，换了一种写法，但是原理相同。
 
 async function as1(){
